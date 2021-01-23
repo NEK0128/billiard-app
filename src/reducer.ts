a3bae8c6d6adb413b48cc9e3053cd4075c7cc5b3
leo1109 <windpass71@yahoo.co.jp>
2021-01-23 21:45:17 +0900

    最大時間の表現を修正

--- src/reducer.ts
+++ src/reducer.ts
@@ -3,26 +3,28 @@ import { TextInputActions } from './actions';
 
 export interface State {
     isPlaying: boolean,
-    clickCount: number
+    clickCount: number,
+    maxCount: number,
 }
 
 export const initialState: State = {
     isPlaying: false,
+    maxCount: 0,
     clickCount: 0
 }
 
 export const Reducer = reducerWithInitialState(initialState)
     .case(TextInputActions.start35CountDown, (state) => {
-        return { ...state, clickCount: 35, isPlaying: true}
+        return { ...state, clickCount: 35, isPlaying: true, maxCount: 35 }
     })
     .case(TextInputActions.start60CountDown, (state) => {
-        return { ...state, clickCount: 60, isPlaying: true }
+        return { ...state, clickCount: 60, isPlaying: true, maxCount: 60 }
     })
     .case(TextInputActions.start180CountDown, (state) => {
-        return { ...state, clickCount: 180, isPlaying: true }
+        return { ...state, clickCount: 180, isPlaying: true, maxCount: 180 }
     })
     .case(TextInputActions.addExtTimeCountDown, (state) => {
-        return { ...state, clickCount: state.clickCount + 30, isPlaying: true }
+        return { ...state, clickCount: state.clickCount + 30, isPlaying: true, maxCount: state.maxCount + 30 }
     })
     .case(TextInputActions.tick, (state) => {
         if (state.isPlaying) {
